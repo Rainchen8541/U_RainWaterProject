d0e9f29bcc05547fe46bfb6e1fc544cae16da853
Rain <rainchen04@gmail.com>
2022-09-24 00:21:48 +0100

    Added Infinite Background
    Change the sprite eventually because it looks crap

---
+++ Assets/BackgroundScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float Parallax = 1.5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        Material Material = meshRenderer.material;
+        Vector2 offset = Material.mainTextureOffset;
+
+        offset.x = transform.position.x / transform.localScale.x / Parallax;
+        offset.y = transform.position.y / transform.localScale.y / Parallax;
+
+        Material.mainTextureOffset = offset;
+    }
+}
--- Assets/Scripts/Particles.cs
+++ Assets/Scripts/Particles.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Particles : MonoBehaviour
 {
     // Start is called before the first frame update
+
     
     void Update()
     {
